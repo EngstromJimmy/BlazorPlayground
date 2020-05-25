6f5053442a4a8579cdc2012c830464b23225121b
Jimmy Engström <jimmy@apeoholic.se>
2020-05-25 10:52:30 +0200

    Added NavigateToNewWindowAsync

--- BlazorPlayground/BlazorPlayground/wwwroot/js/JavaScriptInterop.js
+++ BlazorPlayground/BlazorPlayground/wwwroot/js/JavaScriptInterop.js
@@ -4,3 +4,11 @@ function SetFocus(id) {
     document.getElementById(id).focus();
 }
 //</SetFocus>
+
+//<OpenInNewWindow>
+window.OpenInNewWindow = (url, message) => {
+    var newwindow = window.open('', '_blank');
+    newwindow.document.write(message);
+    newwindow.location.href = url;
+}
+//</OpenInNewWindow>
